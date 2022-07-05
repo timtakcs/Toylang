91acda08585553d01fd65a40e6c6a23fb0a11a5a
timtakcs <timur_takhtarov@houston.nae.school>
2022-07-05 12:11:33 -0500

    man file

--- shell.py
+++ shell.py
@@ -64,24 +64,27 @@ while True:
     # """
 
     #Bubblesort
-    text = """
-        FUNC bubblesort(array, length) {
-            FOR (i = 0; i < length; i++) {
-                FOR (j = 0; j < length - i - 1; j++) { 
-                    IF array[j] > array[j + 1] {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    };
-                };
-            };
+    # text = """
+    #     FUNC bubblesort(array, length) {
+    #         FOR (i = 0; i < length; i++) {
+    #             FOR (j = 0; j < length - i - 1; j++) { 
+    #                 IF array[j] > array[j + 1] {
+    #                     temp = array[j];
+    #                     array[j] = array[j + 1];
+    #                     array[j + 1] = temp;
+    #                 };
+    #             };
+    #         };
 
-            RETURN array;
-        };
+    #         RETURN array;
+    #     };
 
-        testarr = [3, 12, 0, 1, 7, 6];
-        testarr = bubblesort(testarr, 6);
-    """
+    #     testarr = [3, 12, 0, 1, 7, 6];
+    #     testarr = bubblesort(testarr, 6);
+    # """
+
+    with open('main.gs', 'r') as f:
+        text = f.read()
    
     result, error = lang.run(text)
 
